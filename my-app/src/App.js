2c14b28cc874c2c3680bac9a045a6debe06baf70
OlgaGre <grechanyuko@gmail.com>
2024-03-17 18:00:52 +0300

    feat: JSX tag closure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
-//Сделайте так, чтобы результатом рендеринга было следующее:
+//Добавьте в див несколько инпутов, разделенных тегами br.
 
-// <ul>
-// 	<li>text1</li>
-// 	<li>text2</li>
-// 	<li>text3</li>
-// </ul>
 
 function App() {
-	const li1 = <li>text1</li>;
-	const li2 = <li>text2</li>;
-	const li3 = <li>text3</li>;
-  return <ul>
-    {li1}
-    {li2}
-    {li3}
-  </ul>
+	return <div>
+		<input />
+    <br />
+    <input />
+    <br />
+    <input />
+
+	</div>;
 }
 
 export default App;

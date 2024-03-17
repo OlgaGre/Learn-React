3a8afcf98e38f6030c13f6e77c4a8aedcf0f3d5b
OlgaGre <grechanyuko@gmail.com>
2024-03-17 17:03:58 +0300

    fix:

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
+//Сделайте так, чтобы результатом рендеринга был тег ul, в тегах li которого будут стоять элементы массива.
 
 function App() {
-	const str1 = 'text1';
-	const str2 = 'text1';
-	
-	return <div>
-		<p>
-      {str1}
-    </p>
-		<p>
-      {str2}
-    </p>
-	</div>;
+	const arr = [1, 2, 3, 4, 5];
+  let result 
+  function liFromArr()  {
+    for (let i = 0; i < arr.length; i++) {
+  result +=  `<li>${arr[i]} </li>`;
+}
+  }
+  liFromArr()
+return <ul>
+{result}
+</ul>
 }
 
 export default App;

869d519980781a16680f7d58fe35072ba0b45095
OlgaGre <grechanyuko@gmail.com>
2024-03-18 23:04:08 +0300

    JSX funkcijos parametrai

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-//Сделайте так, чтобы по клику на первую кнопку срабатывала первая функция, а по клику на вторую кнопку - вторая функция.
+//Сделайте так, чтобы по клику на первую кнопку алертом выводилось число 1, по клику на вторую кнопку - число 2, а по клику на третью - число 3.
 
 function App() {
-	function show1() {
-		alert(1);
-	}
-	
-	function show2() {
-		alert(2);
-	}
-	
+  function alertNum(num) {
+    alert(num)
+  }
 	return <div>
-		<button onClick={show1}>act1</button>
-		<button  onClick={show2}>act2</button>
+		<button onClick={() => alertNum('1')}>act1</button>
+		<button onClick={() => alertNum('2')}>act2</button>
+		<button onClick={() => alertNum('3')}>act3</button>
 	</div>;
 }
 export default App;
